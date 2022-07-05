c6d39aa050341ca7bc6fe645ae79e9512a91515e
zyf2021 <79312743+zyf2021@users.noreply.github.com>
2022-07-05 19:15:59 +0300

    add express

--- app.js
+++ app.js
@@ -1,21 +1,12 @@
 const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
 const { json } = require('express/lib/response')
 
-const db = require("./models")
 
 const PORT = process.env.PORT || 5000
 
-var corsOptions = {
-    origin:`http://localhost:5001`
-}
 
 const app = express()
 
-global.__basedir = __dirname;
-
-app.use(cors(corsOptions))
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
@@ -29,25 +20,6 @@ app.get('/', function(req, res){
         message:'Working'
     })
 })
-
-require("./routes/uploads.routes")(app)
-require("./routes/applic.routes")(app)
-require("./routes/auth.routes")(app);
-require("./routes/authors.routes")(app)
-require("./routes/books.routes")(app)
-require("./routes/bookmarks.router")(app)
-require("./routes/collections.routes")(app)
-require("./routes/genres.routes")(app)
-require("./routes/news.routes")(app)
-require("./routes/publishings.routes")(app)
-require("./routes/readBooks.routes")(app)
-require("./routes/rewiews.routes")(app)
-require("./routes/series.routes")(app)
-require("./routes/statuses.routes")(app)
-require("./routes/tegs.routes")(app)
-require("./routes/types.routes")(app)
-require("./routes/users.routes")(app)
-
 
 app.listen(PORT, function(){
     console.log(`Server has been started ${PORT}`)
