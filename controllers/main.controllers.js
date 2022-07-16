900de48b6154ae03bfc51268f3eda2da02b5c3f0
zyf2021 <79312743+zyf2021@users.noreply.github.com>
2022-07-16 22:59:10 +0300

    min_max интервал для случайного числа

--- controllers/main.controllers.js
+++ controllers/main.controllers.js
@@ -4,10 +4,28 @@ exports.generate = (req, res) => {
     try {
         var random_number = 0;
         random_number = Math.floor(Math.random()*100);
-        res.send(random_number.toString())
+        res.send(random_number.toString());
     }
     catch(e) {
-        res.status(500).send({message:e.message})
+        res.status(500).send({message:e.message});
+    }  
+}
+
+exports.generate_between = (req, res) => {
+    if ((!req.body.min)&&(!req.body.max)){
+        res.status(400).send({
+            message:"Content empty!"
+        })
+        return;
     }
-    
+    try {
+        const min = Number(req.body.min);
+        const max = Number(req.body.max);
+        var random_number = 0;
+        random_number = Math.floor(Math.random()*(max-min))+min;
+        res.send(random_number.toString());
+    }
+    catch(e){
+      res.status(500).send({message:e.message});
+    }
 }
--- routes/main.routes.js
+++ routes/main.routes.js
@@ -2,6 +2,7 @@ module.exports = app => {
     const controllers = require("../controllers/main.controllers");
     var router = require("express").Router();
     router.get("/generate", controllers.generate);
+    router.get("/generate_between", controllers.generate_between);
    
     app.use('/api/random', router);
   };
